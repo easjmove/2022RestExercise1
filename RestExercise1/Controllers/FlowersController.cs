715d08ea113ae796467e8e80a81db37a9fa701b8
move <move@zealand.dk>
2022-10-12 09:41:46 +0200

    Add project files.

---
+++ RestExercise1/Controllers/FlowersController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestExercise1.Managers;
+using RestExercise1.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace RestExercise1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FlowersController : ControllerBase
+    {
+        private FlowersManagerList _manager = new FlowersManagerList();
+
+        // GET: api/<FlowersController>
+        [HttpGet]
+        public IEnumerable<Flower> Get()
+        {
+            return _manager.GetAll();
+        }
+
+        // GET api/<FlowersController>/5
+        [HttpGet("{id}")]
+        public Flower Get(int id)
+        {
+            return _manager.GetById(id);
+        }
+
+        // POST api/<FlowersController>
+        [HttpPost]
+        public Flower Post([FromBody] Flower newFlower)
+        {
+            return _manager.Add(newFlower);
+        }
+
+        // PUT api/<FlowersController>/5
+        [HttpPut("{id}")]
+        public Flower Put(int id, [FromBody] Flower updates)
+        {
+            return _manager.Update(id, updates);
+        }
+
+        // DELETE api/<FlowersController>/5
+        [HttpDelete("{id}")]
+        public Flower Delete(int id)
+        {
+            return _manager.Delete(id);
+        }
+    }
+}
---
+++ RestExercise1/Controllers/IPAsController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestExercise1.Managers;
+using RestExercise1.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace RestExercise1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IPAsController : ControllerBase
+    {
+        private IPAsManagerList _manager = new IPAsManagerList();
+        // GET: api/<IPAsController>
+        [HttpGet]
+        public IEnumerable<IPA> Get()
+        {
+            return _manager.GetAll();
+        }
+
+        // GET api/<IPAsController>/5
+        [HttpGet("{id}")]
+        public IPA Get(int id)
+        {
+            return _manager.GetById(id);
+        }
+
+        // POST api/<IPAsController>
+        [HttpPost]
+        public IPA Post([FromBody] IPA newIPA)
+        {
+            return _manager.Add(newIPA);
+        }
+
+        // PUT api/<IPAsController>/5
+        [HttpPut("{id}")]
+        public IPA Put(int id, [FromBody] IPA updates)
+        {
+            return _manager.Update(id, updates);
+        }
+
+        // DELETE api/<IPAsController>/5
+        [HttpDelete("{id}")]
+        public IPA Delete(int id)
+        {
+            return _manager.Delete(id);
+        }
+    }
+}
